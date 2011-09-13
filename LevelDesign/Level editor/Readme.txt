154bea6104df5b84ea69c3dd13e2133e4671f2a0
vcarluer <vcarluer@gmail.com>
2011-09-14 01:09:47 +0200

    Inkscape scripts auto deploy

---
+++ LevelDesign/Level editor/Readme.txt
@@ -0,0 +1,2 @@
+Inkscape must be in C:\Program Files (x86)\Inkscape.
+Execute Deploy.bat as administrator.
---
+++ LevelDesign/Level editor/deploy.bat
@@ -0,0 +1,4 @@
+copy "%~dp0slime.inx" "C:\Program Files (x86)\Inkscape\share\extensions\slime.inx"
+copy "%~dp0slime-level-compiler.py" "C:\Program Files (x86)\Inkscape\share\extensions\slime-level-compiler.py"
+copy "%~dp0Slime_level.svg" "C:\Program Files (x86)\Inkscape\share\templates\Slime_level.svg"
+pause
