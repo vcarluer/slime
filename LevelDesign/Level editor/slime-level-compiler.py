c7a84f297d3861d13ee5a804564b614b1dfbbaf6
vcarluer <vcarluer@gmail.com>
2011-09-14 19:29:26 +0200

    Better script

--- LevelDesign/Level editor/slime-level-compiler.py
+++ LevelDesign/Level editor/slime-level-compiler.py
@@ -15,6 +15,9 @@ class Smile( inkex.Effect ):
 	Exemple Inkscape 
 	Cree un nouveau calque et dessine des elements de base
 	"""
+	__yReference = 0
+	__heightReference = 0
+	
 	def __init__(self):
 
 		# Appel du constructeur.
@@ -37,42 +40,47 @@ class Smile( inkex.Effect ):
 		# Creation d'un nouveau calque
 		#root = inkex.etree.Element("MAP")
 		root = self.getElementById("MAP")
-		i=0
+		self.generateNodeAndChilds(root)
+		
+	def generateNodeAndChilds(self, root):
+		self.printNode(root)
 		for child in root:
-			i = i+1			
-			if child.get("type") is not None:
-				# LevelInfo
-				if str(child.get("type")) == 'LevelInfo':
-					referenceY = float(child.get("y"))
-					referenceHeight = float(child.get("height"))
-					print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(child.get("y"))+";"+str(child.get("width"))+";"+str(child.get("height"))
-					continue
-				
-				# Calculate reference Y to inverse items coordinate for GameItem
-				if child.get("height") is not None:
-					y = referenceY + referenceHeight - (float(child.get("y")) + float(child.get("height")))
-				else:
-					y = float(child.get("y"))
+			self.generateNodeAndChilds(child)
+	
+	def printNode(self, child):
+		if child.get("type") is not None:
+			# LevelInfo
+			if str(child.get("type")) == 'LevelInfo':
+				self.__yReference = float(child.get("y"))
+				self.__heightReference = float(child.get("height"))
+				print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(child.get("y"))+";"+str(child.get("width"))+";"+str(child.get("height"))
+				return True;
+			
+			# Calculate new Y based on LevelInfo reference Y and height: Inverse items coordinate for GameItem, origin bottom left
+			if child.get("height") is not None:
+				y = self.__yReference + self.__heightReference - (float(child.get("y")) + float(child.get("height")))
+			else:
+				y = float(child.get("y"))
 
-				# TimeAttack
-				if str(child.get("type")) == 'TimeAttack':
-					print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))+";"+str(child.get("att_levelTime"))+";"+str(child.get("att_criticTime"))
-					continue
-				
-				# BecBunsen
-				if str(child.get("type")) == 'BecBunsen':
-					print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))+";"+str(child.get("att_isOn"))+";"+str(child.get("att_delay"))+";"+str(child.get("id"))
-					continue
+			# TimeAttack
+			if str(child.get("type")) == 'TimeAttack':
+				print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))+";"+str(child.get("att_levelTime"))+";"+str(child.get("att_criticTime"))
+				return True;
+			
+			# BecBunsen
+			if str(child.get("type")) == 'BecBunsen':
+				print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))+";"+str(child.get("att_isOn"))+";"+str(child.get("att_delay"))+";"+str(child.get("id"))
+				return True;
 
-				# Button
-				if str(child.get("type")) == 'Button':
-					print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))+";"+str(child.get("att_target"))+";"+str(child.get("att_resetTime"))
-					continue
+			# Button
+			if str(child.get("type")) == 'Button':
+				print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))+";"+str(child.get("att_target"))+";"+str(child.get("att_resetTime"))
+				return True;
 
-				# Standard Item
-				print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))
-				continue
-						
+			# Standard Item
+			print  str(child.get("type"))+";"+str(child.get("x"))+";"+str(y)+";"+str(child.get("width"))+";"+str(child.get("height"))
+			return True;
+	
 	def affect(self, args=sys.argv[1:]):
 		"""Affect an SVG document with a callback effect"""
 		self.svg_file = args[-1]
