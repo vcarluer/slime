b1323accb0d61808516b234914cae7dee6752a22
Vince <vcarluer@gmail.com>
2011-09-14 17:23:16 +0200

    Logs in deploy script

--- LevelDesign/Level editor/deploy.sh
+++ LevelDesign/Level editor/deploy.sh
@@ -1,7 +1,13 @@
 cd /usr/share/inkscape/extensions
+echo "Deploying slime.in..."
 sudo cp '/home/vince/gamersassociate/Slime/LevelDesign/Level editor/slime.inx' slime.in
+echo "done"
+echo "Deploying slime-level-compiler.py..."
 sudo cp '/home/vince/gamersassociate/Slime/LevelDesign/Level editor/slime-level-compiler.py' slime-level-compiler.py
+echo "done"
 cd /usr/share/inkscape/templates
+echo "Deploying Slime_level.svg..."
 sudo cp '/home/vince/gamersassociate/Slime/LevelDesign/Level editor/Slime_level.svg' Slime_level.svg
-echo "Appuyer sur Entrée pour continuer..."
+echo "done"
+echo "Enter to continue..."
 read a
