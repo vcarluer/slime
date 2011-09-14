8ca3b5b147a73740f85f244a7a71dad93ce71f9d
Vince <vcarluer@gmail.com>
2011-09-14 17:19:13 +0200

    Linux script to deploy auto inkscape extension and templates

---
+++ LevelDesign/Level editor/deploy.sh
@@ -0,0 +1,7 @@
+cd /usr/share/inkscape/extensions
+sudo cp '/home/vince/gamersassociate/Slime/LevelDesign/Level editor/slime.inx' slime.in
+sudo cp '/home/vince/gamersassociate/Slime/LevelDesign/Level editor/slime-level-compiler.py' slime-level-compiler.py
+cd /usr/share/inkscape/templates
+sudo cp '/home/vince/gamersassociate/Slime/LevelDesign/Level editor/Slime_level.svg' Slime_level.svg
+echo "Appuyer sur Entrée pour continuer..."
+read a
