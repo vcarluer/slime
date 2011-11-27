c11e6b9803c03bd06b34f182a713a40857a04b28
vcarluer <vcarluer@gmail.com>
2011-11-27 23:45:22 +0100

    Pause layer design

--- Android/Slime/src/gamers/associate/Slime/layers/PauseLayer.java
+++ Android/Slime/src/gamers/associate/Slime/layers/PauseLayer.java
@@ -3,47 +3,67 @@ package gamers.associate.Slime.layers;
 import gamers.associate.Slime.R;
 import gamers.associate.Slime.game.Level;
 import gamers.associate.Slime.game.Sounds;
+import gamers.associate.Slime.items.custom.MenuSprite;
 
 import org.cocos2d.layers.CCLayer;
 import org.cocos2d.menus.CCMenu;
 import org.cocos2d.menus.CCMenuItem;
 import org.cocos2d.menus.CCMenuItemLabel;
 import org.cocos2d.menus.CCMenuItemSprite;
+import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.nodes.CCLabel;
 import org.cocos2d.nodes.CCSprite;
+import org.cocos2d.types.CGPoint;
 
 public class PauseLayer extends CCLayer {
 	private static String scoreTxt = "MAX: ";
 	private CCMenu menu;
 	private CCMenuItemLabel scoreMaxLabel;
+	private float scale = 0.5f;
+	private float padding = 5f;
 	
 	public PauseLayer() {
-		CCMenuItem label = CCMenuItemLabel.item(getMenuLabel("Pause"), this, "");		
-		
-		this.scoreMaxLabel = CCMenuItemLabel.item(getMenuLabel(scoreTxt), null, "");
+//		CCMenuItem label = CCMenuItemLabel.item(getMenuLabel("Pause"), this, "");		
+//		
+//		this.scoreMaxLabel = CCMenuItemLabel.item(getMenuLabel(scoreTxt), null, "");
 		
 		CCSprite resumeSprite = CCSprite.sprite("control-play.png", true);
 		CCMenuItemSprite resumeMenu = CCMenuItemSprite.item(resumeSprite, resumeSprite, this, "goResume");
+		this.setMenuPos(resumeMenu, 1);
 		
 		CCSprite restartSprite = CCSprite.sprite("control-restart.png", true);
-		CCMenuItemSprite restartMenu = CCMenuItemSprite.item(restartSprite, restartSprite, this, "goRestart");
+		CCMenuItemSprite restartMenu = CCMenuItemSprite.item(restartSprite, restartSprite, this, "goRestart");		
+		this.setMenuPos(restartMenu, 2);
 		
 		CCSprite homeSprite = CCSprite.sprite("control-home.png", true);
 		CCMenuItemSprite homeMenu = CCMenuItemSprite.item(homeSprite, homeSprite, this, "goHome");
+		this.setMenuPos(homeMenu, 3);
 		
-		this.menu = CCMenu.menu(label, this.scoreMaxLabel, resumeMenu, restartMenu, homeMenu);
-		this.menu.alignItemsVertically();
+		this.menu = CCMenu.menu(resumeMenu, restartMenu, homeMenu);
+		// this.menu.alignItemsHorizontally();				
 		
 		this.addChild(this.menu);
 	}
 	
+	private void setMenuPos(CCMenuItem menuItem, int count) {
+		menuItem.setScale(this.scale);
+		
+		float left = - CCDirector.sharedDirector().winSize().getWidth() / 2 + ((MenuSprite.Width * this.scale) + this.padding) / 2 + (MenuSprite.Width * this.scale + this.padding) * (count - 1);
+		float top = CCDirector.sharedDirector().winSize().getHeight() / 2 - ((MenuSprite.Height * this.scale) + 5) / 2;
+		menuItem.setPosition(CGPoint.make(left, top));
+	}
+	
 	public void setMaxScore(String text) {
-		this.scoreMaxLabel.setString(text.toUpperCase());
+		if (this.scoreMaxLabel != null) {
+			this.scoreMaxLabel.setString(text.toUpperCase());
+		}
 	}
 	
 	public void setMaxScore(int scoreMax) {
-		String text = scoreTxt + String.valueOf(scoreMax);
-		this.setMaxScore(text);
+		if (this.scoreMaxLabel != null) {
+			String text = scoreTxt + String.valueOf(scoreMax);
+			this.setMaxScore(text);
+		}
 	}
 	
 	public void goResume(Object sender) {
