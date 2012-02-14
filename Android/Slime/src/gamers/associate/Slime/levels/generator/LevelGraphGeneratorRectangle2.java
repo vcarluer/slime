3349f4236ec091f72f7f55494a7458159e47a4fc
vcarluer <vcarluer@gmail.com>
2012-02-14 13:41:23 +0100

    LevelGenerator2 before tests

--- Android/Slime/src/gamers/associate/Slime/levels/generator/LevelGraphGeneratorRectangle2.java
+++ Android/Slime/src/gamers/associate/Slime/levels/generator/LevelGraphGeneratorRectangle2.java
@@ -1,15 +1,17 @@
 package gamers.associate.Slime.levels.generator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import gamers.associate.Slime.game.LevelDifficulty;
 import gamers.associate.Slime.game.SlimeFactory;
 
 public class LevelGraphGeneratorRectangle2 extends LevelGraphGeneratorRectangle {
-	private static int maxWidth = 24;
-	private static int maxAddHeight = 3;
-	private static int minBossPos = 1;
-	private static Random rand = new Random();
+	private final static int maxWidth = 24;
+	private final static int maxAddHeight = 3;
+	private final static int minBossPos = 1;
+	private final static Random rand = new Random();
 	
 	@Override
 	protected void generateInternal(int maxComplexity,
@@ -17,11 +19,11 @@ public class LevelGraphGeneratorRectangle2 extends LevelGraphGeneratorRectangle 
 		
 		int lgMax = 1;
 		switch (SlimeFactory.GameInfo.getDifficulty()) {
-		default:
-		case LevelDifficulty.Easy: lgMax = maxWidth / 4; break;
-		case LevelDifficulty.Normal: lgMax = maxWidth / 2; break;
-		case LevelDifficulty.Hard: lgMax = maxWidth * 3 / 4; break;
-		case LevelDifficulty.Extrem: lgMax = maxWidth; break;
+			default:
+			case LevelDifficulty.Easy: lgMax = maxWidth / 4; break;
+			case LevelDifficulty.Normal: lgMax = maxWidth / 2; break;
+			case LevelDifficulty.Hard: lgMax = maxWidth * 3 / 4; break;
+			case LevelDifficulty.Extrem: lgMax = maxWidth; break;
 		}
 		
 		int lvlWidth = (int) Math.ceil(SlimeFactory.GameInfo.getLevelNum() * lgMax / SlimeFactory.GameInfo.getLevelMax());
@@ -40,7 +42,201 @@ public class LevelGraphGeneratorRectangle2 extends LevelGraphGeneratorRectangle 
 		// Pick start / end and all other blocks ! Based on complexity? And difficulty?
 		// Boss always at right or at bottom?
 		// At bottom:
-		int bossPos = rand.nextInt(lvlWidth - minBossPos) + minBossPos; 
-	}
+		int bossPos = -1;
+		if (isBoss) {
+			bossPos = rand.nextInt(lvlWidth - minBossPos) + minBossPos; 
+		}
+		
+		LevelGenNode pick = null;
+		for (int col = 0; col < lvlHeight; col++) {
+			for (int row = 0; row < lvlWidth; row++) {
+				// Left col
+				if (col == 0) {
+					if (row == 0) {
+						// can be start
+						if (row == startPos) {
+							pick = this.pickStartBottomLeft();
+						} else {
+							pick = this.pickBottomLeft();
+						}											
+					}
+					if (row > 0 && row < lvlWidth - 1) {
+						if (row == startPos) {
+							pick = this.pickStartMidLeft();
+						} else {
+							pick = this.pickMiddleLeft();
+						}
+						
+					}
+					if (row == lvlWidth - 1) {
+						if (row == startPos) {
+							pick = this.pickStartTopLeft();
+						} else {
+							pick = this.pickTopLeft();
+						}
+						
+					}
+				}
+				
+				// Mid col
+				if (col > 0 && col < lvlHeight - 1) {
+					if (row == 0) {
+						// can be boss
+						if (col == bossPos) {
+							pick = this.pickBossBottomMiddle();
+						} else {
+							pick = this.pickNextGround();
+						}
+					}
+					
+					if (row > 0 && row < lvlWidth - 1) {
+						pick = this.pickMiddle();
+					}
+					
+					if (row == lvlWidth - 1) {
+						pick = this.pickTopMiddle();
+					}
+				}
+				
+				// Right col
+				if (col == lvlHeight - 1) {
+					if (row == 0) {
+						// can be boss
+						if (col == bossPos) {
+							pick = this.pickBossBottomMiddle();
+						} else {
+							// can be exit
+							if (row == endPos) {
+								pick = this.pickExitGroundRightCorner();
+							} else {
+								pick = this.pickGroundRightCorner();
+							}							
+						}
+					}
+					
+					if (row > 0 && row < lvlWidth - 1) {
+						if (row == endPos) {
+							pick = this.pickExitMiddleRight();
+						} else {
+							pick = this.pickMiddleRight();
+						}						
+					}
+					
+					if (row == lvlWidth - 1) {
+						if (row == endPos) {
+							pick = this.pickExitTopRight();
+						} else {
+							pick = this.pickTopRight();
+						}
+					}
+				}
+				
+				if (pick != null) {
+					this.handlePick(pick, false);
+				}
+			}				
+		}
+		
+		this.topCount = lvlHeight - 1;
+		this.rightCount = lvlWidth - 1;
+		// Compute total time et critic time here
+	}	
 
+	protected LevelGenNode pickStartBottomLeft() {
+		LevelGenNode pick = null;
+		List<LevelGenNode> selection = new ArrayList<LevelGenNode>();
+		List<Integer> list = new ArrayList<Integer>();
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Top)));
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Right)));
+		for(LevelGenNode node : this.nodes) {
+			if (node.isLevelStartAndExactlyConnectedTo(list)) {
+				selection.add(node);				
+			}
+		}
+				
+		pick = this.pickFromCompatible(selection);
+		return pick;
+	}
+	
+	protected LevelGenNode pickStartMidLeft() {
+		LevelGenNode pick = null;
+		List<LevelGenNode> selection = new ArrayList<LevelGenNode>();
+		List<Integer> list = new ArrayList<Integer>();
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Top)));
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Right)));
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Bottom)));
+		for(LevelGenNode node : this.nodes) {
+			if (node.isLevelStartAndExactlyConnectedTo(list)) {
+				selection.add(node);				
+			}
+		}
+				
+		pick = this.pickFromCompatible(selection);
+		return pick;
+	}
+	
+	protected LevelGenNode pickStartTopLeft() {
+		LevelGenNode pick = null;
+		List<LevelGenNode> selection = new ArrayList<LevelGenNode>();
+		List<Integer> list = new ArrayList<Integer>();		
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Right)));
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Bottom)));
+		for(LevelGenNode node : this.nodes) {
+			if (node.isLevelStartAndExactlyConnectedTo(list)) {
+				selection.add(node);				
+			}
+		}
+				
+		pick = this.pickFromCompatible(selection);
+		return pick;
+	}
+	
+	protected LevelGenNode pickBottomLeft() {
+		LevelGenNode pick = null;
+		List<LevelGenNode> selection = new ArrayList<LevelGenNode>();
+		List<Integer> list = new ArrayList<Integer>();
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Top)));
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Right)));
+		for(LevelGenNode node : this.nodes) {
+			if (node.isNoSpecialAndExactlyConnectedTo(list)) {
+				selection.add(node);				
+			}
+		}
+				
+		pick = this.pickFromCompatible(selection);
+		return pick;
+	}
+	
+	protected LevelGenNode pickBossBottomMiddle() {
+		LevelGenNode pick = null;
+		List<LevelGenNode> selection = new ArrayList<LevelGenNode>();
+		List<Integer> list = new ArrayList<Integer>();
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Top)));
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Right)));
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Left)));
+		for(LevelGenNode node : this.nodes) {
+			if (node.isLevelBossAndExactlyConnectedTo(list)) {
+				selection.add(node);				
+			}
+		}
+				
+		pick = this.pickFromCompatible(selection);
+		return pick;
+	}
+	
+	protected LevelGenNode pickBossBottomRightCorner() {
+		LevelGenNode pick = null;
+		List<LevelGenNode> selection = new ArrayList<LevelGenNode>();
+		List<Integer> list = new ArrayList<Integer>();
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Top)));		
+		list.addAll(LevelGenNode.getConnectorsFor(LevelGenNode.getMirror(BlocDirection.Left)));
+		for(LevelGenNode node : this.nodes) {
+			if (node.isLevelBossAndExactlyConnectedTo(list)) {
+				selection.add(node);				
+			}
+		}
+				
+		pick = this.pickFromCompatible(selection);
+		return pick;
+	}
 }
