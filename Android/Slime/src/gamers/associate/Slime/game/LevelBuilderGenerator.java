5f195afe4a2d9399d5540ca8b21acba1377c96c1
vcarluer <vcarluer@gmail.com>
2012-02-14 03:04:45 +0100

    debug false

--- Android/Slime/src/gamers/associate/Slime/game/LevelBuilderGenerator.java
+++ Android/Slime/src/gamers/associate/Slime/game/LevelBuilderGenerator.java
@@ -15,7 +15,7 @@ import org.cocos2d.transitions.CCTransitionScene;
 
 public class LevelBuilderGenerator implements ILevelBuilder
 {
-	private static boolean isDebug =  true;
+	private static boolean isDebug =  false;
 	private static int forceDiff = 1;
 	private static int forceLevel = 10;
 	
