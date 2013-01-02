c0c5e0063f3378e67fe7d588f1dbe571399610fa
vcarluer <vcarluer@gmail.com>
2013-01-02 23:37:33 +0100

    selection bug

--- Android/Slime/src/gamers/associate/Slime/layers/ModeItemLayer.java
+++ Android/Slime/src/gamers/associate/Slime/layers/ModeItemLayer.java
@@ -15,17 +15,24 @@ import android.view.MotionEvent;
 public abstract class ModeItemLayer extends CCLayer {
 	private static final int HEIGHT = 533;
 	private static final int WIDTH = 215;
+	
+	private float width;
+	private float height;
 
 	public ModeItemLayer() {
 		CCSprite canvas = CCSprite.sprite(this.getBackgroundPath());
+		float padding = 11f;
 		float scaleX = (CCDirector.sharedDirector().winSize().width / 2f) / WIDTH;
 		float scaleY = CCDirector.sharedDirector().winSize().height / HEIGHT;
 		canvas.setAnchorPoint(0, 0);
 		canvas.setScaleX(scaleX);
 		canvas.setScaleY(scaleY);
+		this.width = WIDTH * scaleX;
+		this.height = HEIGHT * scaleY;
+		
 		this.addChild(canvas);
 		CCLabel label = SlimeFactory.getLabel(this.getTitle());
-		label.setPosition(WIDTH * scaleX / 2f, HEIGHT * scaleY / 2f);
+		label.setPosition(this.width / 2f, this.height / 2f);
 		this.addChild(label);
 		this.setIsTouchEnabled(true);
 	}
@@ -40,10 +47,10 @@ public abstract class ModeItemLayer extends CCLayer {
 		float y = CCDirector.sharedDirector().winSize().getHeight() - event.getY();
 		float parentX = this.getParent().getPosition().x;
 		float parentY = this.getParent().getPosition().y;
-		float realX = this.getPosition().x + parentX;
-		float realY = this.getPosition().y + parentY;
-		boolean inrectX = x < realX + WIDTH / 2f && x > realX - HEIGHT / 2f;
-		boolean inrectY = y < realY + WIDTH / 2F && y > realY - HEIGHT / 2f;
+		float realX = this.getPosition().x + parentX + this.width / 2f;
+		float realY = this.getPosition().y + parentY + this.height / 2f;
+		boolean inrectX = x < realX + this.width / 2f && x > realX - this.height / 2f;
+		boolean inrectY = y < realY + this.width / 2F && y > realY - this.height / 2f;
 		if (inrectX && inrectY) {
 			this.select();
 			return true;
