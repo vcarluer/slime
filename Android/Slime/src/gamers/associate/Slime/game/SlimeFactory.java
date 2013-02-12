0c7bcc659cac98cd1b800de7b832858b54375186
vcarluer <vcarluer@gmail.com>
2013-02-12 15:43:45 +0100

    10 e_tr!

--- Android/Slime/src/gamers/associate/Slime/game/SlimeFactory.java
+++ Android/Slime/src/gamers/associate/Slime/game/SlimeFactory.java
@@ -66,11 +66,11 @@ import com.badlogic.gdx.physics.box2d.World;
 	public static boolean IsLevelSelectionShowButtons = false;
 
 	public static boolean IsLevelDebugMode = false;
-	public static boolean IsForceDiffDebug =  false;
+	public static boolean IsForceDiffDebug =  true;
 	public static int ForceDiff = 8;
 	public static int ForceLevel = 1;
-	public static boolean IsDebugBlocOn = false;
-	public static String ForceBlockPath = "blocsRectangle/e_br_10.slime";	
+	public static boolean IsDebugBlocOn = true;
+	public static String ForceBlockPath = "blocsRectangle/e_tr_3.slime";	
 	public static final boolean IsForceMaxSurvival = false;
 	public static final int MaxSurvival = LevelDifficulty.Hard;
 	public static final boolean IsForceMaxWorld = false;
