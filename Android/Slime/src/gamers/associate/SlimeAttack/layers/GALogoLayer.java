9516b749b59711d0286d8f7b72dda2c66682f4ad
vcarluer <vcarluer@gmail.com>
2013-07-03 00:38:47 +0200

    Do not load removed res

--- Android/Slime/src/gamers/associate/SlimeAttack/layers/GALogoLayer.java
+++ Android/Slime/src/gamers/associate/SlimeAttack/layers/GALogoLayer.java
@@ -189,7 +189,7 @@ import android.annotation.SuppressLint;
 						SpriteSheetFactory.add("controls", true, SpriteSheetFactory.zDefault);
 						break;
 					case 1:
-						SpriteSheetFactory.add("decor", true, SpriteSheetFactory.zDefault);
+						// Nothing here
 						break;
 					case 2:					
 						SpriteSheetFactory.add("items", Level.zFront);
@@ -201,7 +201,7 @@ import android.annotation.SuppressLint;
 						SpriteSheetFactory.add("red", Level.zTop);
 						break;
 					case 5:
-						SpriteSheetFactory.add("slimydbz", Level.zFront);
+						// Nothing here
 						break;
 					case 6:
 						SpriteSheetFactory.add("glasswork", Level.zMid);
